034bfa455590f485ed801e0b9dabb8d3d23a815a
Taylor Plante <taylorplante@MacBook-Pro-63.local>
2017-05-14 10:15:36 -0700

    installed dependencies for react, started webpack configuration

---
+++ webpack.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+	entry: './app.js',
+
+	output: {
+		filename: 'bundle.js'
+	},
+
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			}
+		]
+	}
+};
